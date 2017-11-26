f458e44a1c4440111d46b3fdfe1a8578f75983eb
Vladimir Kolesnik <vkolesni@gmail.com>
2017-11-26 21:03:29 +0200

    Add pip and aws

--- Dockerfile
+++ Dockerfile
@@ -1,10 +1,10 @@
 #https://access.redhat.com/documentation/en-us/red_hat_enterprise_linux_atomic_host/7/html/getting_started_with_containers/using_red_hat_base_container_images_standard_and_minimal
-
 FROM centos:latest
 
 ARG PACKER_VERSION="1.1.2"
 ARG TERRAFORM_VERSION="0.11.0"
 ARG VAGRANT_VERSION="2.0.1"
+ARG AWSCLI_VERSION="1.12.0"
 
 RUN yum -y install unzip \
 	make \
@@ -27,6 +27,14 @@ ENV TERRAFORM_BIN=/opt/terraform/terraform
 # VAGRANT
 RUN rpm -i https://releases.hashicorp.com/vagrant/${VAGRANT_VERSION}/vagrant_${VAGRANT_VERSION}_x86_64.rpm
 ENV VAGRANT_BIN=vagrant
+
+# PIP
+RUN curl -L -o /tmp/get-pip.py https://bootstrap.pypa.io/get-pip.py
+RUN python /tmp/get-pip.py
+RUN rm -f /tmp/get-pip.py
+
+# AWSCLI
+RUN pip install "awscli==${AWSCLI_VERSION}"
 
 # CLEANUP
 RUN rm -f /tmp/packer.zip /tmp/terraform.zip
--- source.sh
+++ source.sh
@@ -1,12 +1,21 @@
 #!/bin/bash
+set -e
+
 IMAGE_NAME=""
 source "$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"/image-config.sh
 
+function vte-bash()
+{
+    echo "Running vte-bash command with args: $@"
+    #[ -f ~/.aws/credentials ] && docker cp -f ~/.aws/credentials
+	docker run --net=host -it --rm --entrypoint /bin/bash "${IMAGE_NAME}" -c "bash"
+}
+
 function vte-make()
-(
+{
     echo "Running vte-make command with args: $@"
 	docker run --net=host --privileged -it -v $(pwd):$(pwd) --rm --entrypoint /bin/bash "${IMAGE_NAME}" -c "cd $(pwd); make $@"
-)
+}
 
 function vte-packer()
 {
@@ -24,4 +33,19 @@ function vte-terraform-wrapper()
 {
     echo "Running vte-terraform-wrapper command with args: $@"
 	docker run --net=host --privileged -it -v $(pwd):$(pwd) --rm --entrypoint /bin/bash "${IMAGE_NAME}" -c "cd $(pwd); ./terraform-wrapper.sh $*"
+}
+
+function vte-aws()
+{
+    echo "Running vte-aws command with args: $@"
+    #export AWS_CREDENTIAL_PROFILES_FILE=path/to/credentials_file
+    #~/.aws/credentials
+    # AWS_DEFAULT_REGION
+    # AWS_ACCESS_KEY_ID
+    # AWS_SECRET_ACCESS_KEY
+    # [default]
+    # aws_access_key_id={YOUR_ACCESS_KEY_ID}
+    # aws_secret_access_key={YOUR_SECRET_ACCESS_KEY}
+
+	docker run --net=host -it --rm --entrypoint /bin/bash "${IMAGE_NAME}" -c "aws $@"
 }
