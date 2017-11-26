fa600b0eab110e14aba1076264218a199e30fc10
Vladimir Kolesnik <vkolesni@gmail.com>
2017-11-26 21:03:48 +0200

    Rename aliases.sh to source.sh

--- source.sh
+++ source.sh
@@ -1,45 +1,45 @@
 #!/bin/bash
-set -e
 
 IMAGE_NAME=""
+DOCKER_BIN="sudo docker"
 source "$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"/image-config.sh
 
 function vte-bash()
 {
     echo "Running vte-bash command with args: $@"
     #[ -f ~/.aws/credentials ] && docker cp -f ~/.aws/credentials
-	docker run --net=host -it --rm --entrypoint /bin/bash "${IMAGE_NAME}" -c "bash"
+	${DOCKER_BIN} run --net=host -it --rm --entrypoint /bin/bash "${IMAGE_NAME}" -c "bash"
 }
 
 function vte-make()
 {
     echo "Running vte-make command with args: $@"
-	docker run --net=host --privileged -it -v $(pwd):$(pwd) --rm --entrypoint /bin/bash "${IMAGE_NAME}" -c "cd $(pwd); make $@"
+	${DOCKER_BIN} run --net=host --privileged -it -v $(pwd):$(pwd) --rm --entrypoint /bin/bash "${IMAGE_NAME}" -c "cd $(pwd); make $@"
 }
 
 function vte-packer()
 {
     echo "Running vte-packer command with args: $@"
-	docker run --net=host --privileged -it -v $(pwd):$(pwd) --rm --entrypoint /bin/bash "${IMAGE_NAME}" -c "cd $(pwd); /opt/packer/packer $@"
+	${DOCKER_BIN} run --net=host --privileged -it -v $(pwd):$(pwd) --rm --entrypoint /bin/bash "${IMAGE_NAME}" -c "cd $(pwd); /opt/packer/packer $@"
 }
 
 function vte-terraform()
 {
     echo "Running vte-terraform command with args: $@"
-	docker run --net=host --privileged -it -v $(pwd):$(pwd) --rm --entrypoint /bin/bash "${IMAGE_NAME}" -c "cd $(pwd); /opt/terraform/terraform $@"
+	${DOCKER_BIN} run --net=host --privileged -it -v $(pwd):$(pwd) --rm --entrypoint /bin/bash "${IMAGE_NAME}" -c "cd $(pwd); /opt/terraform/terraform $@"
 }
 
 function vte-terraform-wrapper()
 {
     echo "Running vte-terraform-wrapper command with args: $@"
-	docker run --net=host --privileged -it -v $(pwd):$(pwd) --rm --entrypoint /bin/bash "${IMAGE_NAME}" -c "cd $(pwd); ./terraform-wrapper.sh $*"
+	${DOCKER_BIN} run --net=host --privileged -it -v $(pwd):$(pwd) --rm --entrypoint /bin/bash "${IMAGE_NAME}" -c "cd $(pwd); ./terraform-wrapper.sh $*"
 }
 
 function vte-aws()
 {
     echo "Running vte-aws command with args: $@"
     #export AWS_CREDENTIAL_PROFILES_FILE=path/to/credentials_file
-    #~/.aws/credentials
+    #~/.aws/d
     # AWS_DEFAULT_REGION
     # AWS_ACCESS_KEY_ID
     # AWS_SECRET_ACCESS_KEY
@@ -47,5 +47,5 @@ function vte-aws()
     # aws_access_key_id={YOUR_ACCESS_KEY_ID}
     # aws_secret_access_key={YOUR_SECRET_ACCESS_KEY}
 
-	docker run --net=host -it --rm --entrypoint /bin/bash "${IMAGE_NAME}" -c "aws $@"
+	${DOCKER_BIN} run --net=host -it --rm --entrypoint /bin/bash "${IMAGE_NAME}" -c "aws $@"
 }
